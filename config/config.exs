00b7d7e62e07837159168245f989648e5eeb09a0
viniciusilveira <marcos.silveira@magnetis.com.br>
2020-12-24 09:55:31 -0300

    Configure phoenix-swagger to documentation api

--- config/config.exs
+++ config/config.exs
@@ -19,6 +19,16 @@ config :blog, BlogWeb.Endpoint,
   pubsub_server: Blog.PubSub,
   live_view: [signing_salt: "0pIuLu9b"]
 
+config :blog, :phoenix_swagger,
+  swagger_files: %{
+    "priv/static/swagger.json" => [
+      router: BlogWeb.Router,
+      endpoint: BlogWeb.Endpoint
+    ]
+  }
+
+config :phoenix_swagger, json_library: Jason
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Blog.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers() ++ [:phoenix_swagger],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -41,7 +41,8 @@ defmodule Blog.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_swagger, "~> 0.8"}
     ]
   end
 
