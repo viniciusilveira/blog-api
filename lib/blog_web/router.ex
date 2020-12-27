d16cc6872ee523bd8b0c7307aca99a80d3783cce
Vinicius Silveira <viniciusilveira@users.noreply.github.com>
2020-12-26 22:18:58 -0300

    Revert "Adiciona phoenix_swagger"

--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -8,20 +8,4 @@ defmodule BlogWeb.Router do
   scope "/api", BlogWeb do
     pipe_through :api
   end
-
-  scope "/api/swagger" do
-    forward "/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :blog, swagger_file: "swagger.json"
-  end
-
-  def swagger_info do
-    %{
-      info: %{
-        version: "1.0",
-        title: "Blog API",
-        description: "API Documentation for Blog API"
-      },
-      consumes: ["application/json"],
-      produces: ["application/json"]
-    }
-  end
 end
